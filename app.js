382a03928011b907ad4682c4b603e3a1eef5ad58
clarejacky <clare.jacky@gmail.com>
2015-04-30 17:34:30 -0500

    working multi city sort of

--- app.js
+++ app.js
@@ -1,50 +1,51 @@
 /**
  * Created by ClareJacky on 4/30/15.
  */
-var cityData = null;
-var cityHtml = null;
+var cityData;
+var cityHtml;
 
-var tokyoName;
-var tokyoPop;
-var tokyoArea;
 
-var newyorkName;
-var newyorkPop;
-var neworkArea;
+var i = 0;
 
-var torontoName;
-var torontoPop;
-var torontoArea;
+function getData () {
+
+    for (i = 0; i < cityData.cities.length; i++) {
+        $(".more-stuff").append(cityHtml);
+
+        var locationNames = cityData.cities[i].location;
+        var locationPops = cityData.cities[i].population;
+        var locationAreas = cityData.cities[i].area;
+        $(".more-stuff").children().children().first().append("<p>" + locationNames + "</p>");
+        $(".more-stuff").children().children().first().append("<p>" + locationPops + "</p>");
+        $(".more-stuff").children().children().first().append("<p>" + locationAreas + "</p>");
+
+    }
+}
 
 $(document).ready(function(){
-
-    $("#get-info-btn").on("click", function (){
-            $.get('location.html', function(data) {
-                cityHtml = data;
-                $(".more-stuff").append(data);
-            });
-
-            $.get('data.json', function(data){
-                cityData = data;
-                tokyoName = data.locations[0].location;
-                tokyoPop = data.locations[0].population;
-                tokyoArea = data.locations[0].area;
-                $(".more-stuff").children().first().append("<p>"+tokyoName+"</p>");
-                $(".more-stuff").children().first().append("<p>"+tokyoPop+"</p>");
-                $(".more-stuff").children().first().append("<p>"+tokyoArea+"</p>");
-
-                tokyoName = data.locations[0].location;
-                tokyoPop = data.locations[0].population;
-                tokyoArea = data.locations[0].area;
-
-            });
-
-        //best to change scope with variable
-
+    $.get('data.json', function (data) {
+        cityData = data;
+        console.log(cityData);
 
     });
+    $.get('location.html', function (data) {
+        cityHtml = data;
+        console.log(data);
+
+    });
+
+
+        $("#get-info-btn").on("click", function () {
+            getData();
+        });
+
 
     $(".more-stuff").on("click", "#remove", function () {
-        $(this).parent().parent().empty();
+        $(this).closest().parent().empty();
     });
 });
+
+
+//tokyoName = data.locations[0].location;
+//tokyoPop = data.locations[0].population;
+//tokyoArea = data.locations[0].area;
