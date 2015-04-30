f14db4958ed241d0e0c9bc5f6805a3367d3f7274
clarejacky <clare.jacky@gmail.com>
2015-04-30 14:53:56 -0500

    add additional html and json

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
 /**
  * Created by ClareJacky on 4/30/15.
  */
+var cityData = null;
+var cityHtml = null;
+
+
+$(document).ready(function(){
+    console.log(cityData, cityHtml);
+    //$.get
+    //$.ajax
+    $("#get-info-btn").on("click", function (){
+        if (cityHtml == null) {
+            $.get('location.html', function(data) {
+                cityHtml = data;
+                console.log("it worked");
+                $(".more-stuff").append(data);
+            });
+        } else {
+            console.log ("you already got it");
+        }
+
+
+
+            $.get('data.json', function(data){
+                console.log("second");
+                var firstCity = data.locations[0].location;
+                $(".more-stuff").children().children().append("<p>"+firstCity+"</p>");
+            });
+
+    });
+
+
+
+
+
+});
